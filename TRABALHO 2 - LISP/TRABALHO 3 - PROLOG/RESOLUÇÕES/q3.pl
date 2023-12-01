6dff592a4ad81a0fce62346e58f0516e12d0d5cf
Gabriela Marculino <36650561+GabrielaMarculino@users.noreply.github.com>
2023-12-01 14:48:40 -0400

    Delete TRABALHO 2 - LISP/TRABALHO 3 - PROLOG directory

--- TRABALHO 2 - LISP/TRABALHO 3 - PROLOG/RESOLUÇÕES/q3.pl
+++
@@ -1,7 +0,0 @@
-%Trabalho realizdo por Cauê Mendonça e Gabriela Marculino
-
-% Predicado imprimeLista que recebe uma lista como argumento
-imprimeLista([]). % Caso base: se a lista está vazia, não faz nada
-imprimeLista([Head|Tail]) :- % Caso recursivo: divide a lista em cabeça e cauda
-    format('~w~n', [Head]), % Imprime a cabeça da lista
-    imprimeLista(Tail). % Chama recursivamente com a cauda da lista
